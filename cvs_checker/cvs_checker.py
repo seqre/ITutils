7fa96840ee6d7f802e0b39f45410d3af1fab3170
Marek 'seqre' Grzelak <git@seqre.dev>
2021-04-01 14:26:15 -0400

    CVS checker: add headless option

--- cvs_checker/cvs_checker.py
+++ cvs_checker/cvs_checker.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -7,7 +8,10 @@ CVS_LINK = 'https://www.cvs.com/immunizations/covid-19-vaccine'
 INDIANA_STATE_XPATH = '/html/body/content/div/div/div/div[3]/div/div/div[2]/div/div[5]/div/div/div/div/div/div[1]/div[2]/div/div[2]/div/div/div/div/div[1]/ul/li[14]/div/a'
 FORT_WAYNE_XPATH = '/html/body/div[2]/div/div[4]/div/div/div/div/div/div[1]/div[2]/div/div/div[2]/div/div[6]/div/div/table/tbody/tr[4]'
 
-driver = webdriver.Firefox()
+options = Options()
+options.headless = True
+
+driver = webdriver.Firefox(options=options)
 driver.get(CVS_LINK)
 
 indiana_state = driver.find_element_by_xpath(INDIANA_STATE_XPATH)
